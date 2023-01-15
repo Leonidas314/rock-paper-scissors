aa12ffff46dabc75fb397a16188511c148ce8c16
leon <leondellafiore2016@gmail.com>
2023-01-15 19:43:45 -0300

    Añadido de animacion a botones, cpu selector, algo de javascript

--- javascript.js
+++ javascript.js
@@ -6,8 +6,7 @@ let introContainer = document.querySelector('.intro-container');
 let imagesContainer = document.querySelector('.images-container');
 
 /*Cambiar la propiedad css de un elemento para que no sea visible*/
-document.getElementById("images-container").style.display = 'none'
-
+document.getElementById('images-container').style.display = 'none';
 
 /*Funcion para capturar evento click en  variable boton
 se remueve la variable "introContainer" y se cambia la propiedad de
@@ -22,7 +21,54 @@ elementon con id ="images-container" a "flex"*/
  
 })
 
-/*imagesContainer.addEventListener('mouseover' , function(){
+/*Capturar el evento click sobre las imagenes de piedra, papel o tijera, diferenciando cada una*/
 
-    console.log("Pasó");
-})*/
+let playerSelectors = document.querySelector('#player-select');
+
+let piedra = document.querySelector('#piedra');
+let papel = document.querySelector('#papel');
+let tijera = document.querySelector('#tijera');
+
+/*playerSelectors.addEventListener('click',function(){
+    console.log("sape")
+});*/
+
+function Capturar(){
+
+    
+    if(    piedra.addEventListener('click',function(){
+            console.log("Piedra");
+
+        let playerChoice = "Piedra";
+        return playerChoice;
+        console.log(playerChoice);
+
+        })
+    ) 
+    if(
+        papel.addEventListener('click',function(){
+            console.log("papel")
+          let playerChoice = "Papel";
+          return playerChoice;
+        })
+    )
+    if(
+        tijera.addEventListener('click',function(){
+            console.log("tijera")
+            let playerChoice = "Tijera"
+            return playerChoice;
+        })
+    )
+        
+    return playerChoice;
+}
+
+playerChoice = Capturar;
+console.log(playerChoice);
+/*Funcion que recibe una variable desde la seleccion del jugador y la compara con la seleccion de la maquina*/
+
+function seleccionComparación(playerChoice,cpuChoice){
+
+
+
+}
